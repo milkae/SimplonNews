8eb91b86569a7404f6460c87e738fd607827deac
Floriane <floriane_grondin@hotmail.com>
2016-04-20 14:38:24 +0200

    Css et js de la précédente refonte layout

--- config/app.php
+++ config/app.php
@@ -156,7 +156,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         Laravel\Socialite\SocialiteServiceProvider::class,
-        // Collective\Html\HtmlServiceProvider::class,
 
     ],
 
@@ -204,8 +203,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         'Socialite' => Laravel\Socialite\Facades\Socialite::class,
-        // 'Form'      => Collective\Html\FormFacade::class,
-        // 'Html'      => Collective\Html\HtmlFacade::class,
 
     ],
 
--- public/js/app.js
+++ public/js/app.js
@@ -5,4 +5,12 @@ $(function(){
 		e.preventDefault();
 		$(this).next('.hidden').show();
 	});
+	$('a.reply').click(function(e){
+		e.preventDefault();
+		$(this).parent().children('.reply.hidden').toggle();
+	});
+	$('a.edit').click(function(e){
+		e.preventDefault();
+		$(this).parent().children('.edit.hidden').toggle();
+	});
 });
