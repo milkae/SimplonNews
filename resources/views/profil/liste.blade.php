608a4fee60b01c7fe332a7686e2c6945f64d01bd
Floriane <floriane_grondin@hotmail.com>
2016-04-25 15:34:41 +0200

    Ajout affichage des rôles

--- resources/views/profil/liste.blade.php
+++ resources/views/profil/liste.blade.php
@@ -5,12 +5,18 @@
 		<h3 class="ui header">Liste des Utilisateurs</h3>
 		<div class="ui middle aligned celled list">
 		@foreach($users as $user)
-		  <div class="item">
-		    <img class="ui avatar image" src="{{$user->avatar?$user->avatar:'http://lorempixel.com/600/600/cats'}}">
-		    <div class="content">
-		      <a class="header" href="{{ URL::route('user.profile', [$user->id]) }}">{{ $user->name }}</a>
-		    </div>
-		  </div>
+			<div class="item">
+			    <img class="ui avatar image" src="{{$user->avatar?$user->avatar:'http://lorempixel.com/600/600/cats'}}">
+			    <div class="content">
+			      <a class="header" href="{{ URL::route('user.profile', [$user->id]) }}">{{ $user->name }}</a>
+			      {{ $user->karma }}
+			    </div>
+			    <div class="right floated">
+				    @foreach($user->roles as $role)
+						<div class="ui label {{$user->hasRole($role->name)?$role->color:''}}">{{ ucfirst(trans($role->name)) }}</div>
+					@endforeach
+				</div>
+			</div>
 		@endforeach
     @endif
 @endsection
--- resources/views/profil/profil.blade.php
+++ resources/views/profil/profil.blade.php
@@ -1,105 +1,133 @@
 @extends('layouts.app')
-	@section('content')
-	<!-- employeur travail github photo -->
-	<div class="mid-content" style="position: relative; top: 15%;">
-		<div class="ui center aligned container stackable grid">
-			<div class="nine wide column">
-				<div class="ui labels">
-				@foreach($user->roles as $role)
-					<div class="ui label {{$user->hasRole($role->name)?$role->color:''}}">{{ ucfirst(trans($role->name)) }}</div>
-				@endforeach
-				</div>
-				<div class="ui container stackable segments">
-				@if(url()->current() == url('edit/profil'))
-				<form action="{{ URL::route('user.profile.edit') }}" method="POST">
-				{!! csrf_field() !!}
-				@endif
-					<div class="ui horizontal container stackable segments">
-						<div class="ui left aligned attached padded segment">
-							<div class="header dividing">{{ $user->name }}</div>
-							@if(url()->current() == url('edit/profil'))
-							<button class="ui compact icon pink inverted button show-next"><i class="setting icon"></i></button>
-							<input type="text" name="name" value="{{$user->name}}" class="hidden">
-							@endif
+@section('content')
+<!-- employeur travail github photo -->
+<div class="ui center aligned container stackable grid">
+	<div class="nine wide column">
+		<div class="ui labels">
+		@foreach($user->roles as $role)
+			<div class="ui label {{$user->hasRole($role->name)?$role->color:''}}">{{ ucfirst(trans($role->name)) }}</div>
+		@endforeach
+		</div>
+		@if(url()->current() == url('edit/profil'))
+		<form class="ui form" action="{{ URL::route('user.profile.edit') }}" method="POST">
+		{!! csrf_field() !!}
+		@endif
+		<div class="ui container stackable segments">
+			<div class="ui horizontal container stackable segments">
+				<div class="ui left aligned attached padded segment">
+					@if(url()->current() == url('edit/profil'))
+					<button class="ui right floated compact icon pink inverted button show-next"><i class="setting icon"></i></button>
+					@endif
+					<div class="header dividing">
+						<p>{{ $user->name }}</p>
+						<div class="ui field hidden">
+							<input type="text" name="name" value="{{$user->name}}" class="">
 						</div>
-						@if (Auth::check() && Auth::user()->id == $user->id && url()->current() !== url('/edit/profil'))
-						<div class="ui right aligned attached segment">
-							<a href="{{ URL::route('user.profile.edit') }}"><button class="ui compact labeled icon pink inverted button"><i class="setting icon"></i> Edit </button></a>
-						</div>
-						@endif
-						@if(url()->current() == url('edit/profil'))
-						<div class="ui right aligned attached segment">
-							<input type="submit" value="Enregistrer les modifications" class="ui compact labeled icon pink inverted button">
-						</div>
-						@endif
 					</div>
-					<div class="ui segment">
-						<div class="ui container stackable grid">
-							<div class="eight wide column">
-								<img src="{{$user->avatar?$user->avatar:'http://lorempixel.com/600/600/cats'}}" alt="Image de Profil" class="ui large rounded image">
+				</div>
+				<div class="ui left aligned attached segment">
+						<div class=" header">Karma</div>
+						@if($user->karma > 1000)
+						<i class="icon diamond big pink"></i>
+						@endif
+				@if (Auth::check() && Auth::user()->id == $user->id && url()->current() !== url('/edit/profil'))
+					<a href="{{ URL::route('user.profile.edit') }}"><button class="ui right floated compact labeled icon pink inverted button"><i class="setting icon"></i> Edit </button></a>
+				@endif
+				</div>
+				@if(url()->current() == url('edit/profil'))
+				<div class="ui right aligned attached segment">
+					<input type="submit" value="Enregistrer les modifications" class="ui compact labeled icon pink inverted button">
+				</div>
+				@endif
+			</div>
+			<div class="ui segment">
+				<div class="ui container stackable grid">
+					<div class="eight wide column">
+						<img src="{{$user->avatar?$user->avatar:'http://lorempixel.com/600/600/cats'}}" alt="Image de Profil" class="ui large rounded image">
+					</div>
+					<div class="eight wide left aligned verry padded column">
+						<div class="ui list">
+							<div class="item">
+								@if(url()->current() == url('edit/profil'))
+								<button class="ui right floated compact icon pink inverted button show-next"><i class="setting icon"></i></button>
+								@endif
+								<div class="content">
+									<div class="header">Nom</div>
+									{{$user->nom}}
+									<div class="ui field hidden">
+										<input type="text" name="nom" value="{{$user->nom}}" class="">
+									</div>
+								</div>
 							</div>
-							<div class="eight wide left aligned verry padded column">
-								<div class="ui list">
-									<div class="item">
-										<div class="header">Nom</div>
-										{{$user->nom}}
-										@if(url()->current() == url('edit/profil'))
-										<button class="ui compact icon pink inverted button show-next"><i class="setting icon"></i></button>
-										<input type="text" name="nom" value="{{$user->nom}}" class="hidden">
-										@endif
-									</div>
-									<div class="ui divider"></div>
+							<div class="ui divider"></div>
 
-									<div class="item">
-										<div class="header">Prénom</div>
-										{{$user->prenom}}
-										@if(url()->current() == url('edit/profil'))
-										<button class="ui compact icon pink inverted button show-next"><i class="setting icon"></i></button>
-										<input type="text" name="prenom" value="{{$user->prenom}}" class="hidden">
-										@endif
+							<div class="item">
+								@if(url()->current() == url('edit/profil'))
+								<button class="ui right floated compact icon pink inverted button show-next"><i class="setting icon"></i></button>
+								@endif
+								<div class="content">
+									<div class="header">Prénom</div>
+									{{$user->prenom}}
+									<div class="ui field hidden">
+										<input type="text" name="prenom" value="{{$user->prenom}}" class="">
 									</div>
-									<div class="ui divider"></div>
+								</div>
+							</div>
+							<div class="ui divider"></div>
 
-									<div class="item">
-										<div class="header"><i class="travel icon"></i>Profession</div>
-										{{$user->job}}
-										@if(url()->current() == url('edit/profil'))
-										<button class="ui compact icon pink inverted button show-next"><i class="setting icon"></i></button>
-										<input type="text" name="job" value="{{$user->job}}" class="hidden">
-										@endif
+							<div class="item">
+								@if(url()->current() == url('edit/profil'))
+								<button class="ui right floated compact icon pink inverted button show-next"><i class="setting icon"></i></button>
+								@endif
+								<div class="content">
+									<div class="header">Profession</div>
+									{{$user->job}}
+									<div class="ui field hidden">
+										<input type="text" name="job" value="{{$user->job}}" class="">
 									</div>
-									<div class="ui divider"></div>
+								</div>
+							</div>
+							<div class="ui divider"></div>
 
-									<div class="item">
-										<div class="header">Employeur</div>
-										{{$user->employeur}}
-										@if(url()->current() == url('edit/profil'))
-										<button class="ui compact icon pink inverted button show-next"><i class="setting icon"></i></button>
-										<input type="text" name="employeur" value="{{$user->employeur}}" class="hidden">
-										@endif
+							<div class="item">
+								@if(url()->current() == url('edit/profil'))
+								<button class="ui compact right floated icon pink inverted button show-next"><i class="setting icon"></i></button>
+								@endif
+								<div class="content">
+									<div class="header">Employeur</div>
+									{{$user->employeur}}
+									<div class="ui field hidden">
+										<input type="text" name="employeur" value="{{$user->employeur}}" class="">
 									</div>
-									<div class="ui divider"></div>
+								</div>
+							</div>
+							<div class="ui divider"></div>
 
-									<div class="item">
-										<div class="header">Github</div>
+							<div class="item">
+								@if(url()->current() == url('edit/profil'))
+								<button class="ui compact right floated icon pink inverted button show-next"><i class="setting icon"></i></button>
+								@endif
+								<div class="content">
+									<div class="header">Github</div>
+									<div class="description">
 										<button class="ui circular github icon button">
 											<i class="github icon"></i>
 										</button>
 										<a href="{{$user->github}}">{{ $user->name }}</a>
-										@if(url()->current() == url('edit/profil'))
-										<button class="ui compact icon pink inverted button next"><i class="setting icon"></i></button>
-										<input type="text" name="github" value="{{$user->github}}" class="hidden">
-										@endif
-										<div class="right floated header">Karma : {{$user->karma}}</div>
+									</div>
+									<div class="ui field hidden">
+										<input type="text" name="github" value="{{$user->github}}" class="">
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-				@if(url()->current() == url('edit/profil'))
-				</form>
-				@endif
 				</div>
 			</div>
 		</div>
+		@if(url()->current() == url('edit/profil'))
+		</form>
+		@endif
 	</div>
+</div>
+@endsection
