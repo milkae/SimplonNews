6b4eda86312ced40d387c83d7e308bd643dda00d
Floriane <floriane_grondin@hotmail.com>
2016-06-02 23:07:38 +0200

    Changement taille du texte des commentaires

---
+++ database/migrations/2016_06_02_210154_change_content_size_comments_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeContentSizeCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+          DB::statement('ALTER TABLE `comments` MODIFY COLUMN `content` TEXT');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
